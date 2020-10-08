15816ae10e3637a93485a61682bf50524b1127fa
Timothy Cook <tim.wade.cook@gmail.com>
2020-10-08 12:39:04 -0400

    switch avatar to react-native-elements

--- components/Atomic/Avatar.js
+++ components/Atomic/Avatar.js
@@ -1,20 +1,21 @@
 import React from "react";
-import UserAvatar from "react-native-user-avatar";
+import { Avatar, Accessory } from "react-native-elements";
 import { SharedElement } from "react-navigation-shared-element";
 
 import useTheme from "../../hooks/useTheme";
 
-export default function Avatar({ user, size = 24, style }) {
+export default function AvatarComponent({ user, size = 24, style, children }) {
   const theme = useTheme();
+
   return (
-    <SharedElement id={`avatar-${user._id}`}>
-      <UserAvatar
-        style={{ borderWidth: 0.2, borderColor: theme.white, ...style }}
-        size={size}
-        name={user.name}
-        bgColor={theme.iconDefault}
-        src={user.avatar}
-      />
-    </SharedElement>
+    <Avatar
+      rounded
+      containerStyle={{ borderWidth: 0.2, borderColor: theme.white, ...style }}
+      size={size}
+      title={user.name[0].toUpperCase()}
+      source={{ uri: user.avatar }}
+    >
+      {children}
+    </Avatar>
   );
 }
